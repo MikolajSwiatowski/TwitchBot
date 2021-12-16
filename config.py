6ba60f772face06dc7a21272174fa03fe73ae0bb
MikolajSwiatowski <mikolaj.swiatowski@gmail.com>
2021-12-16 20:24:16 +0100

    message

---
+++ config.py
@@ -0,0 +1,3 @@
+OAUTH_TOKEN = 'oauth:ln898ad95llxr7gp5b0t84eptw3a9g'
+
+
---
+++ pokedex.py
@@ -0,0 +1,10 @@
+class Pokemon:
+    def __init__(self, pokename, pokeinfo):
+        self.pokename = pokename
+        self.pokeinfo = pokeinfo
+    def give_pokeinfo(self):
+        return self.pokeinfo
+
+pikachu = 'Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.'
+
+#NOTE THIS FEATURE IS STILL WORK IN PROGRESS
